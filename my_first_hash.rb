b70ccf0a64153cdb380a1658d9002a58ab065fe5
remote-torrent-6747 <abbyc@sky.com>
2020-12-22 22:11:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,6 +11,7 @@ def shipping_manifest
   }
 end
 
+
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
@@ -18,9 +19,16 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+
 
 end
+
+
+
+
+
+
+
 
 def adding
   shipping_manifest = {
